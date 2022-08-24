641f8463fdf0555d98b2358b4c1eb30e75288efb
Jens Bröder <j.broeder@fz-juelich.de>
2022-08-24 12:22:06 +0200

    Add first version of dashboard with more advanced barplot and survey data

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,42 +5,62 @@ import random
 import os
 from random import randint
 from os.path import dirname, join
-
+import pandas as pd
+from bokeh.models import Panel, Tabs
 from bokeh.layouts import column, row
-from bokeh.models import Button
+from bokeh.models import Dropdown
 from bokeh.palettes import RdYlBu3
+from bokeh.palettes import Category20c
+
 from bokeh.plotting import curdoc
 from bokeh.models import ColumnDataSource, Div, Select
 from bokeh.plotting import figure as bokeh_figure
 from bokeh.models import CheckboxButtonGroup, CustomJS
+from bokeh.models import MultiChoice
+from bokeh.models import Paragraph
 from masci_tools.vis.bokeh_plots import bokeh_scatter, bokeh_multi_scatter
-from .plots import bokeh_barchart, bokeh_piechart
+from .plots import bokeh_barchart, bokeh_piechart, add_legend_outside, bokeh_corr_plot#, bokeh_barchart2
+from .analysis import calculate_crosstab, prepare_data_research_field, filter_dataframe
+
+
+pwd = os.getcwd() # todo better use the absolute location of this file...
+datafilepath= join(pwd, 'dashboard/data/20211130_HMCCommSurvey_clean.csv')
 
 desc = Div(text=open(join(dirname(__file__), "description.html")).read(), sizing_mode="stretch_width")
 
 pwd = os.getcwd()
-questions = open(join(pwd, 'dashboard/data/questions.txt')).read().split('\n')
+questions = open(join(pwd, 'dashboard/data/barchart_allowed.txt')).read().split('\n')
 QUESTION_MAP = {question : i for i, question in enumerate(questions)}
+FILTER_OPTIONS = open(join(pwd, 'dashboard/data/filters.txt')).read().split('\n')
 
-question_select = Select(title="Question", value="Question 1",
+# question select could also be a slider?
+question_select = Select(title="Question", value="careerLevel",
                options=questions)
-filter_select = Select(title="Data Filter", value="All",
-               options=open(join(pwd, 'dashboard/data/filters.txt')).read().split('\n'))
+filter_select = Select(title="Filter data", value="All",
+               options=FILTER_OPTIONS)
+
+multi_choice = MultiChoice(title="Filter data", value=["All", "Physics"], options=FILTER_OPTIONS)
+multi_choice.js_on_change("value", CustomJS(code="""
+    console.log('multi_choice: value=' + this.value, this.toString())
+"""))
 
 #LABELS = open(join(pwd, 'dashboard/data/filters.txt')).read().split('\n')
 
-#filter_select = CheckboxButtonGroup(labels=LABELS, active=[0, 1])
-#filter_select.js_on_click(CustomJS(code="""
-#    console.log('checkbox_button_group: active=' + this.active, this.toString())
-#"""))
-chart_select = Select(title="Chart type", value="bar",
-               options=['bar', 'pie'])
+#chart_select = Select(title="Chart type", value="bar",
+#               options=['bar', 'pie'], description="Plot the same Data as Piechart")
+
+lang_select = Select(title="Language", value="en",
+               options=['en', 'de'], description="Change the language of the Dashboard.")
 
 # Controls 2
-question_select2 = Select(title="Question X-Axis", value="Question 1",
-               options=questions)
-question_select3 = Select(title="Question Y-Axis", value="Question 2",
-               options=questions)
+question_select2 = Select(title="Question X-Axis", value="careerLevel",
+               options=questions, description="Select the question which results should be displayed on the X-Axis.")
+question_select3 = Select(title="Question Y-Axis", value="docStructured",
+               options=questions, description="Select the question which results should be displayed on the Y-Axis.")
+
+menu = ['Barchart, Diffchart, Correlation Plot']
+button_bar = Dropdown(label="Add a chart", button_type="success", menu=menu)
+#button_corr = Button(label="Add correlation chart display", button_type="success")
 
 TOOLTIPS=[
     ("Title", "@title"),
@@ -48,81 +68,158 @@ TOOLTIPS=[
     ("Number of Answers", "@y")
 ]
 
-
-x = [str(i) for i in range(7)]
-xs = [x,x,x,x,x]
+# colors of research areas
+#ra_colors = {'All': "#75968f", 'Chemistry': "#a5bab7", 'Earth Science':"#c9d9d3", 'Engineering Science': "#e2e2e2", 'Life Science': "#dfccce", 'Mathematics': "#ddb7b1", 'Other': "#cc7878", 'Physics': "#933b41", 'Psychology': "#550b1d"}
+re = ['All', 'Chemistry', 'Earth Science', 'Engineering Science', 'Life Science', 'Mathematics', 'Other', 'Physics', 'Psychology']
+re_c = Category20c[len(re)]
+ra_colors = {field: re_c[i] for i, field in enumerate(re)}
+#x = [str(i) for i in range(7)]
+#xs = [x,x,x,x,x]
 #xs = [[f'1 pli bla blub {i}' for i in x], [f'2 pli bla blub {i}' for i in x], [f'3 pli bla blub {i}' for i in x], [f'4 pli bla blub {i}' for i in x], [f'5 pli bla blub {i}' for i in x]]
-ys = [[randint(0, 1200) for i in x], [randint(0, 1200) for i in x], [randint(0, 1200) for i in x],[randint(0, 1200) for i in x], [randint(0, 1200) for i in x]]
+#ys = [[randint(0, 1200) for i in x], [randint(0, 1200) for i in x], [randint(0, 1200) for i in x],[randint(0, 1200) for i in x], [randint(0, 1200) for i in x]]
+#counts2 = sum(zip(*ys), ())
+#factors = [(str(ques), ans) for ques in range(len(xs)) for ans in xs[0]] # factors have to be unique, and strings
+#df_test3 = ColumnDataSource(data=dict(value=factors, counts=counts2))
 
-
+survey_data = pd.read_csv(datafilepath)
 
 #fig = bokeh_figure(height=600, width=700, title="", toolbar_location='right', tooltips=TOOLTIPS, sizing_mode="scale_both")
-source = ColumnDataSource(data=dict(value=xs[0], counts=ys[0]))
-source2 = ColumnDataSource(data=dict(value=xs[1], counts=ys[1]))
+source = ColumnDataSource(survey_data)
+source_corr = ColumnDataSource(survey_data)
 
-fig = bokeh_barchart(source, factors=xs[0])
-fig2 = bokeh_barchart(source2, factors=xs[1])
+def select_data(question_select=question_select, multi_choice=multi_choice):
+    """Select the data to display"""
+
+    question = question_select.value
+    #index = QUESTION_MAP[question]
+    q_index = question
+    to_exclude = []
+    #TODO map the question to index/key
+    #TODO there should be a certain display order, i.e mappings needed
+    #TODO, also some things are excluded
+
+    data_filters = multi_choice.value
+
+    # need something like [index: index_name, full question: '', title; '', 'key order' : [], 'skipped keys':[]]
+    
+    # for now this is greedy, if to slow think of another way
+    # we want to display anything in terms of researchArea, todo better was to d filter, generalize this
+    df = filter_dataframe(survey_data, include=[q_index, "researchArea"], exclude=[(q_index, to_exclude)])
+    
+    exclude = []
+    for field in re:
+        if field not in data_filters:
+            exclude.append(field)
+    for area in exclude:
+        df = df[df["researchArea"] != area]
+    data, y_keys = prepare_data_research_field(df, q_index)
+    #df = pd.DataFrame.from_dict(data)
+
+
+    #data_filters = y_keys
+    ydata_spec = {}
+    colors = []
+    for key in y_keys:
+        if key in data_filters:
+            colors.append(ra_colors[key])
+    
+    ydata_spec['y_keys'] = data_filters
+    ydata_spec['colors'] = colors
+    ydata_spec['legend_labels'] = data_filters
+    selected = ColumnDataSource(data=data)#dict(value=values, counts=counts, factors=values, color=colors, legend_labels=data_filters))#, title=question))
+    ydata_spec = ColumnDataSource(data=ydata_spec)
+    return selected, ydata_spec, question
+
+
+
+def select_data_corr(question_select2=question_select2, question_select3=question_select3):
+    """Select the data to display"""
+    question = question_select2.value
+    question2 = question_select3.value
+    
+    #q1_key = QUESTION_MAP[question]
+    q1_key = question
+    q2_key = question2
+    
+    # If this is slow to calculate each time, it might make sense to calculate all of these from the at start up.
+    # i.e n^2 tables
+    cross_tab = calculate_crosstab(survey_data, q1_key, q2_key) 
+    print(cross_tab.keys())
+    #totals = survey_data[question].value_counts()
+    counts = cross_tab['percentage']
+    values = cross_tab['percentage']#cross_tab['total']
+    # Bokeh plots need a ColumnDataSource, but this can be initialized from a pandas
+    selected = ColumnDataSource(data=dict(x_values=values, y_values=values, counts=counts))#, title=question))
+    return selected
+
+
+
+#def select_chart(chart_select=chart_select):
+##   """Select how the data should be displayed"""
+#    chart_type = chart_select.value.strip()
+#    return chart_type
+
+def update():
+    """Update the charts"""
+    df, ydata_spec, question = select_data()
+    #charttype = select_chart(chart_select=chart_select)
+    
+    #source.data = dict(value=df.data['value'], counts=df.data['counts'], legend_labels=df.data['legend_labels'])
+    source = df
+    # TODO: always plot piechart in seperate tab
+    #if charttype == 'pie':
+    #    fig = bokeh_piechart(source)#, figure=figure)
+    #    row1.children[1] = fig
+    #else:
+        #print(y_keys, df.data['colors'])
+    y_keys = ydata_spec.data['y_keys']
+    fill_colors = ydata_spec.data['colors']
+    fig = bokeh_barchart(source, y=y_keys, factors=y_keys, fill_color=fill_colors, title=question)#df.data['factors'], legend_labels='legend_labels')#, figure=figure)
+    row1.children[1] = fig
+    #add_legend_outside(fig)
+
+def update_corr():
+    """Update the charts"""
+    df = select_data_corr()    
+    fig_corr = bokeh_corr_plot(df)
+    row1.children[2] = fig_corr
+
+
+start_display_data, ydata_spec, question= select_data()
+y_keys = ydata_spec.data['y_keys']
+fill_colors = ydata_spec.data['colors']
+fig = bokeh_barchart(start_display_data, y=y_keys, factors=y_keys, legend_labels=y_keys, fill_color=fill_colors, title=question)#factors=start_display_data.data['factors'])
+
+start_corr_data = select_data_corr()
+fig_corr = bokeh_corr_plot(start_corr_data)
+#fig2 = bokeh_barchart(source_corr)
 
 #fig2 = bokeh_multi_scatter(source2, marker_size=[int(x)+6 for x in xs[1])
 
 
-def select_data():
-    """Select the data to display"""
-    question = question_select.value
-    index = QUESTION_MAP[question]
-    selected = ColumnDataSource(data=dict(value=xs[index], counts=ys[index], factors=xs[index]))#, title=question))
-    return selected
+def generate_bar_controls():
+    controls_bar = [question_select, multi_choice]
+    return controls_bar
 
-def select_data2():
-    """Select the data to display"""
-    question = question_select2.value
-    question2 = question_select3.value
-    index = QUESTION_MAP[question]
-    selected = ColumnDataSource(data=dict(value=xs[index], counts=ys[index], factors=xs[index]))#, title=question))
-    return selected
+def generate_corr_controls():
+    controls_corr = [question_select2, question_select3]
+    return controls_corr
 
-
-
-def select_chart():
-    """Select how the data should be displayed"""
-    charttype = chart_select.value.strip()
-    return charttype
-
-def update():
-    """Update the charts"""
-    df = select_data()
-    charttype = select_chart()
-    
-    source.data = dict(value=df.data['value'], counts=df.data['counts'])
-    
-    if charttype == 'pie':
-        fig = bokeh_piechart(source)#, figure=figure)
-        row1.children[1] = fig
-    else:
-        fig = bokeh_barchart(source, factors=df.data['factors'])#, figure=figure)
-        row1.children[1] = fig
-    #show(fig)
-
-def update2():
-    """Update the charts"""
-    df = select_data2()    
-    source2.data = dict(value=df.data['value'], counts=df.data['counts'])
-    fig2 = bokeh_barchart(source2, factors=df.data['factors'])
-
-controls = [question_select, filter_select, chart_select]
-
-inputs = column(*controls, width=200)
-row1 = row(inputs, fig)
-for control in controls:
+# Dashboard layout
+controls_bar = generate_bar_controls()
+inputs = column(*controls_bar, width=200)
+for control in controls_bar:
     control.on_change('value', lambda attr, old, new: update())
-first = column(desc, row1 , sizing_mode="scale_both")
+
+controls_corr = generate_corr_controls()
+for control in controls_corr:
+    control.on_change('value', lambda attr, old, new: update_corr())
+inputs_corr = column(*controls_corr, width=200)
 
 
-controls2 = [question_select2, question_select3]
-for control in controls2:
-    control.on_change('value', lambda attr, old, new: update2())
-inputs2 = column(*controls2, width=200)
-layout = column(first, row(inputs2, fig2), sizing_mode="scale_both")
+row1 = row(inputs, fig, fig_corr, inputs_corr)
+first = column(desc, row1, sizing_mode="scale_both")
+layout = column(first, row(button_bar), sizing_mode="scale_both")
 
 curdoc().add_root(layout)
 curdoc().title = "HMC Survey Dashboard"
--- dashboard/plots.py
+++ dashboard/plots.py
@@ -5,8 +5,13 @@ from bokeh.plotting import figure as bokeh_figure
 from bokeh.palettes import Category20c
 from bokeh.transform import factor_cmap
 from bokeh.transform import cumsum
-from bokeh.models import FactorRange
+from bokeh.models import FactorRange, Legend
+from bokeh.models import LinearColorMapper
+from bokeh.transform import transform
+from bokeh.models import HelpTool
+from bokeh.models import HoverTool
 
+from bokeh.transform import dodge
 
 # for tests
 #import pandas as pd
@@ -19,9 +24,10 @@ from bokeh.models import FactorRange
 #ys = [[random.randint(0, 1200) for i in x], [random.randint(0, 1200) for i in x], [random.randint(0, 1200) for i in x],[random.randint(0, 1200) for i in x], [random.randint(0, 1200) for i in x]]
 
 # bokeh bar plot
-def bokeh_barchart(df, x='value', y='counts', factors=None, figure=None, title='', width=0.9,  xlabel='Answers', ylabel='Number of answers', palette=Category20c, fill_color='color'):
+'''
+def bokeh_barchart(df, x='value', y='counts', factors=None, figure=None, title='This is a title', width=0.9,  xlabel='Answers', ylabel='Number of answers', palette=Category20c, fill_color='color', description='For more information about the HMC survey click here.', redirect='https://helmholtz-metadaten.de/en/pages/structure-governance'):
     """Plot an interactive bar chart with bokeh"""
-    
+    legend_it = []
     if isinstance(df, ColumnDataSource):
         xdata = df.data[x]
         if not 'color' in df.column_names:
@@ -31,37 +37,130 @@ def bokeh_barchart(df, x='value', y='counts', factors=None, figure=None, title='
         xdata = df[x]
         if not 'color' in df.columns:
             df['color'] = Category20c[len(xdata)] # ! if len(xdata)>20 this fails
+    
+    help_t = HelpTool(description=description, redirect=redirect)
 
+    tools = 'hover,wheel_zoom,box_zoom,undo,reset,save'
     if figure is None:
         if factors is not None:
             fig = bokeh_figure(plot_height=600, plot_width=600,
                title=title,
-               toolbar_location='right',
+               toolbar_location='above',
                x_range=FactorRange(factors=factors),
-               tools='',#'hover',
+               tools=tools,#'hover',
                tooltips=[('Data', f'@{x}'), ('Count', f'@{y}')])
         else:
             fig = bokeh_figure(plot_height=600, plot_width=600,
                title=title,
-               toolbar_location='right',
-               tools='',#'hover',
+               toolbar_location='above',
+               tools=tools,#'hover',
                tooltips=[('Data', f'@{x}'), ('Count', f'@{y}')])
     else:
         fig = figure
     #if factors is not None:
     #    fig.x_range=FactorRange(factors=factors)
     
+    fig.add_tools(help_t)
     fig.vbar(x=x, top=y, width=width, source=df, line_color="white", fill_color=fill_color)#factor_cmap('x', palette=palette, factors=factors, start=1, end=2))
+    fig.y_range.start = 0
+    fig.x_range.range_padding = 0.1
+    fig.toolbar.logo = None
+    fig.xaxis.major_label_orientation = 1
+    fig.xgrid.grid_line_color = None
+    fig.yaxis.axis_label = ylabel
+    fig.xaxis.axis_label = xlabel
+    fig.xgrid.grid_line_color = None
+
+
+    #fig.legend.location = "left"
+    #fig.legend.orientation = "horizontal"
+    #fig.legend.click_policy="hide"
+
+    return fig
+
+def bokeh_barchart2(df, x=['value'], y=['counts'], factors=None, figure=None, data_visible=[True], title='', width=0.9,  xlabel='Answers', ylabel='Number of answers', palette=Category20c, fill_color='color', legend_labels=None):
+    """Plot an interactive bar chart with bokeh"""
+
+    if figure is None:
+        if factors is not None:
+            fig = bokeh_figure(plot_height=600, plot_width=600,
+               title=title,
+               toolbar_location='above',
+               #x_range=FactorRange(factors=factors),
+               tools='',#'hover',
+               tooltips=[('Data', f'@{x}'), ('Count', f'@{y}')])
+        else:
+            fig = bokeh_figure(plot_height=600, plot_width=600,
+               title=title,
+               toolbar_location='above',
+               tools='',#'hover',
+               tooltips=[('Data', f'@{x}'), ('Count', f'@{y}')])
+    else:
+        fig = figure
+    
+    if isinstance(x, list):
+        if not 'color' in df.column_names:
+            if isinstance(palette, dict):
+                x_color = palette[len(x)]
+        else:
+            x_color = df.data['color']
+        position = []
+        step = width + 0.05
+        nvisible = data_visible.count(True)
+        if nvisible%2 == 0:
+            start = -step*nvisible/2 + step/2.0
+        elif nvisible==1:
+            start = 0.0
+        else:
+            start = nvisible//2 * -step
+        displayed_pos = [start + i*step for i in range(nvisible)]
+        ind = 0
+        for visible in data_visible:
+            if not visible:
+                position.append(0.0)
+            else:
+                position.append(displayed_pos[ind])
+                ind = ind+1
+        for i, data in enumerate(x):
+            fig.vbar(x=dodge(data, position[i], range=fig.x_range), top=y[i], source=df,
+                   width=width, color=x_color[i], legend_label=data, visible=data_visible[i])
+    else:
+        if isinstance(df, ColumnDataSource):
+            xdata = df.data[x]
+            if not 'color' in df.column_names:
+                df.data['color'] = palette[len(xdata)]
+        else:
+            xdata = df[x]
+            if not 'color' in df.columns:
+                df['color'] = palette[len(xdata)]
+        fig.vbar(x=x, top=y, width=width, source=df, line_color="white", fill_color=fill_color)#factor_cmap('x', palette=palette, factors=factors, start=1, end=2))
+    
     fig.y_range.start = 0
     fig.x_range.range_padding = 0.1
     fig.xaxis.major_label_orientation = 1
     fig.xgrid.grid_line_color = None
     fig.yaxis.axis_label = ylabel
     fig.xaxis.axis_label = xlabel
+    fig.xgrid.grid_line_color = None
+
+    fig.toolbar.logo = None
+    #fig.legend.location = "left"
+    #fig.legend.orientation = "horizontal"
+    #fig.legend.click_policy="hide"
 
     return fig
+'''
 
-
+def add_legend_outside(fig):
+    """
+    Move legend to the outside of a figure
+    """
+    #fig.legend.visible = False
+    #legent_it = fig.legend.items
+    #legend = Legend(items=legent_it)
+    legend = fig.legend
+    fig.add_layout(legend, 'right')
+    return fig
 # test
 #df_test = pd.DataFrame(data=dict(value=xs[0], counts=ys[0]))
 #fig = bokeh_barchart(df_test, factors=xs[0])
@@ -74,6 +173,57 @@ def bokeh_barchart(df, x='value', y='counts', factors=None, figure=None, title='
 #palette = Category20c[len(ys[0])]
 #fig = bokeh_barchart(df_test3, factors=factors, fill_color=factor_cmap('value', palette=palette, factors=xs[0], start=1, end=2))
 #show(fig)
+
+
+def bokeh_barchart(df, x='x_value', y=['y_value'], factors=None, figure=None, data_visible=[True], title='', 
+                    width=0.1,  xlabel='', ylabel='Number of answers', palette=Category20c, 
+                    fill_color=None, legend_labels=None, description='For more information about the HMC survey click here.', redirect='https://helmholtz-metadaten.de/en/pages/structure-governance'):
+    y_keys = y
+    source = df
+
+    help_t = HelpTool(description=description, redirect=redirect)
+    tools = 'wheel_zoom,box_zoom,undo,reset,save'
+    fig = bokeh_figure(x_range=source.data[x], title=title, #y_range=(0, 280), 
+           height=550, width=1200, toolbar_location='above', tools=tools)
+
+    fig.add_tools(help_t)
+    
+    nvisible = len(y_keys)
+    step = width + 0.05
+    if nvisible%2 == 0:
+        start = -step*nvisible/2 + step/2.0
+    elif nvisible==1:
+        start = 0.0
+    else:
+        start = nvisible//2 * -step
+    
+    position = [start + i*step for i in range(len(y))]        
+    tooltips=[(f'{x}', f'@{x}')]
+    bars = []
+    for i, y in enumerate(y_keys):
+        bar = fig.vbar(x=dodge(x, position[i], range=fig.x_range), top=y, source=source,
+           width=width, color=fill_color[i], legend_label=y)
+        tooltips.append((f'{y}', '@{' + str(y) + '}'))
+        bars.append(bar)
+
+    # How the data was given, there is not a way for the hover tool to display a single value
+    hover = HoverTool(tooltips=tooltips,renderers=bars)
+    fig.add_tools(hover)    
+    fig.x_range.range_padding = 0.1
+    fig.xgrid.grid_line_color = None
+    fig.legend.location = "top_left"
+    fig.legend.orientation = "horizontal"
+    fig.y_range.start = 0
+    fig.xaxis.major_label_orientation = 1
+    fig.yaxis.axis_label = ylabel
+    fig.xaxis.axis_label = xlabel
+    fig.toolbar.logo = None
+    fig.legend.location = "top_right"
+    fig.legend.orientation = "vertical"
+    fig.legend.click_policy="hide"
+
+    return fig
+
 
 
 # bokeh piechart    
@@ -97,9 +247,9 @@ def bokeh_piechart(df, x='value', y='counts', figure=None, radius=0.8, title='')
     df.data['angle'] = ydata / sum(list(ydata)) * 2 * pi
 
     if figure is None:
-        fig = bokeh_figure(plot_height=600, plot_width=600,
+        fig = bokeh_figure(height=600, width=600,
                title=title,
-               toolbar_location='right',
+               toolbar_location='above',
                tools='',#hover',
                tooltips=[('Data', f'@{x}'),
                          ('Percent', '@percent{0.00%}'), 
@@ -114,15 +264,55 @@ def bokeh_piechart(df, x='value', y='counts', figure=None, radius=0.8, title='')
             end_angle=cumsum('angle'),
             line_color='white',
             fill_color='color',
-            legend_field='xlabel',
+            legend_field=x,
             source=df)
-    
+    fig.toolbar.logo = None
     fig.axis.axis_label = None
     fig.axis.visible = False
     fig.grid.grid_line_color = None
+    fig.legend.location = "right"
+    #fig.legend.orientation = "horizontal"
+    #fig.legend.click_policy="hide"
+
     return fig
 
 # test
 #df_test = pd.DataFrame(data=dict(value=xs[0], counts=ys[0], xlabel=xs[0]))
 #fig = bokeh_piechart(df_test)
 #show(fig)
+
+
+def bokeh_corr_plot(df, x='x_values', y='y_values',  figure=None, title='', markersize='markersize',  xlabel='Answers', ylabel='Number of answers', alpha=None):
+    """Plot an interactive circle with bokeh"""
+
+    if figure is None:
+        fig = bokeh_figure(height=600, width=600,
+               title=title,
+               toolbar_location='above',
+               tools='',#'hover',
+               tooltips=[('Data', f'@{x}'), ('Count', f'@{y}')])
+    else:
+        fig = figure
+    if alpha is None:
+        alpha = 1.0
+    colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+    mapper = LinearColorMapper(palette=colors, low=df.data[y].min(), high=df.data[y].max())   
+    fig.circle(source=df, x=x, y=y, radius = 0.9)#size=markersize, fill_color={'field': 'region', 'transform': color_mapper}, fill_alpha=alpha)
+    #line_color='#7c7e71',
+    #line_width=0.5,
+    #line_alpha=0.5,
+    #legend_group='region')
+    
+    fig.y_range.start = 0
+    fig.x_range.range_padding = 0.1
+    fig.xaxis.major_label_orientation = 1
+    #fig.xgrid.grid_line_color = None
+    fig.yaxis.axis_label = ylabel
+    fig.xaxis.axis_label = xlabel
+    fig.toolbar.logo = None
+
+    #fig.legend.location = "left"
+    #fig.legend.orientation = "horizontal"
+    #fig.legend.click_policy="hide"
+
+    return fig
