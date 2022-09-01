b977b55ca559b25a389f920ec56e5505cc432549
Jens Bröder <j.broeder@fz-juelich.de>
2022-09-01 08:33:09 +0200

    Add filter for methods and cleanup bokeh from main

--- dashboard/main.py
+++ dashboard/main.py
@@ -62,7 +62,7 @@ questions_keys = open(join(pwd, 'dashboard/data/barchart_allowed.txt')).read().s
 questions = [map_qkey_to_question(key) for key in questions_keys]
 #QUESTION_MAP = {question : i for i, question in enumerate(questions)}
 FILTER_OPTIONS = open(join(pwd, 'dashboard/data/filters.txt')).read().split('\n')
-MARGINS = (10,10,10,10)
+FILTER_OPTIONS_METHOD = open(join(pwd, 'dashboard/data/filters_methods.txt')).read().split('\n')
 
 # Data displayed on startup:
 START_DATA_BAR = map_qkey_to_question("careerLevel")
@@ -89,12 +89,12 @@ source_corr = ColumnDataSource(survey_data)
 #### Control widgets
 # Bar chart
 # D: question select could also be a slider?
-question_select = pn.widgets.Select(name="Select a Question", value=START_DATA_BAR,
-    options=questions)#, description="Select the survey question, which results should be displayed.", margin=MARGINS)
+question_select = pn.widgets.Select(name="Select a Question from the survey", value=START_DATA_BAR,
+    options=questions)#, description="Select the survey question, which results should be displayed.")
 
-multi_choice = pn.widgets.MultiChoice(name="Filter data by research field", value=["All"], options=FILTER_OPTIONS)#, 
-    #description="Select which data subset to be displayed.", margin=MARGINS)
-multi_choice = pn.widgets.MultiChoice(name="Filter data by research field", value=["All"], options=FILTER_OPTIONS)
+multi_choice = pn.widgets.MultiChoice(name="Filter by research field", value=["All"], options=FILTER_OPTIONS)#, 
+    #description="Select which data subset to be displayed.")
+multi_choice_method = pn.widgets.MultiChoice(name="Filter by data generation method", value=["All"], options=FILTER_OPTIONS_METHOD)
 
 
 # D: better than a Select would be to use the HMC template for this...
@@ -114,7 +114,6 @@ plot_aspect_ratio_select = pn.widgets.Select(name="Filter data", value="Cum. Sum
                options=FILTER_OPTIONS)
 menu = ['Barchart', 'Correlation Plot']#('Barchart', 'bar_chart'), ('Correlation Plot', '(corr_plot')]
 button_bar = pn.widgets.Select(name="Add a chart", options=menu)
-#button_corr = Button(label="Add correlation chart display", button_type="success")
 
 TOOLTIPS=[
     ("Title", "@title"),
@@ -136,12 +135,13 @@ def select_data():#question_select=question_select, multi_choice=multi_choice):
     #TODO, also some things are excluded
 
     data_filters = multi_choice.value
-
+    data_filters_method = multi_choice_method.value
     # need something like [index: index_name, full question: '', title; '', 'key order' : [], 'skipped keys':[]]
     
     # for now this is greedy, if to slow think of another way
     # we want to display anything in terms of researchArea, todo better was to d filter, generalize this
     df = filter_dataframe(survey_data, include=[q_index, "researchArea"], exclude=[(q_index, to_exclude)])
+    #df = filter_dataframe(df, include=[q_index, "researchArea"],  exclude=[(q_index, to_exclude)])
     data_all = get_all_values(df, q_index)
 
     exclude = []
@@ -184,9 +184,15 @@ def select_data_corr():#question_select2=question_select2, question_select3=ques
     x_range = HCS_orderedCats[q1_key] #list(survey_data[q1_key].value_counts().keys())
     y_range = HCS_orderedCats[q2_key] #list(survey_data[q2_key].value_counts().keys())
 
+
+    data_filters = multi_choice.value
+    data_filters_method = multi_choice_method.value
+
     # If this is slow to calculate each time, it might make sense to calculate all of these 
     # at start up. i.e n^2 tables
-    cross_tab = calculate_crosstab(survey_data, q1_key, q2_key)
+    df = filter_dataframe(survey_data, include=[q1_key, q2_key, "researchArea"], exclude=[])
+
+    cross_tab = calculate_crosstab(df, q1_key, q2_key)
     # marker size is radius, we want the Area to be proportional to the value
     # we also scale the markers with the max values are to small, or depending how many Cat, or 
     # figure width...
@@ -204,9 +210,6 @@ def select_data_corr():#question_select2=question_select2, question_select3=ques
 
     title = f'{q1_key} in dependence to {q2_key}'
     # Bokeh plots need a ColumnDataSource, but this can be initialized from a pandas
-    #selected = ColumnDataSource(data=dict(x_values=cross_tab[q1_key], y_values=cross_tab[q2_key],
-    #    markersize=cross_tab['percentage'], total=))
-
     selected = ColumnDataSource(cross_tab)
 
     display_options = {'x_range' : x_range, 'y_range' : y_range, 
@@ -214,33 +217,20 @@ def select_data_corr():#question_select2=question_select2, question_select3=ques
     return selected, display_options, marker_scale
 
 
-
-#def select_chart(chart_select=chart_select):
-##   """Select how the data should be displayed"""
-#    chart_type = chart_select.value.strip()
-#    return chart_type
-
 def update(target, event, charttype=1):
-    """Update the charts"""
-    print(event)
-    ob_index=0
-    df, ydata_spec, question = select_data()
-    #charttype = select_chart(chart_select=chart_select)
+    """Update the charts
     
-    #source.data = dict(value=df.data['value'], counts=df.data['counts'], legend_labels=df.data['legend_labels'])
+    The target will be replaced, the event contains what interaction was trickered.
+    Here we assume value change, because no others where allowed.
+    """
+    print(event)
+    df, ydata_spec, question = select_data()
     source = df
-    # TODO: always plot piechart in seperate tab
-    #if charttype == 'pie':
-    #    fig = bokeh_piechart(source)#, figure=figure)
-    #    row1.children[1] = fig
-    #else:
-        #print(y_keys, df.data['colors'])
+
     y_keys = ydata_spec.data['y_keys']
     fill_colors = ydata_spec.data['colors']
     fig = bokeh_barchart(source, y=y_keys, factors=y_keys, legend_labels=y_keys, fill_color=fill_colors, title=f'Q: {question}', orientation='vertical')#df.data['factors'], legend_labels='legend_labels')#, figure=figure)
-    #fig.margin = MARGINS
     #fig2 = bokeh_barchart(source, y=y_keys, factors=y_keys, legend_labels=y_keys, fill_color=fill_colors, title=question, orientation='horizontal')#'horizontal')#df.data['factors'], legend_labels='legend_labels')#, figure=figure)
-    #fig2.margin = MARGINS
 
     #tab1 = ("Vertical Bar chart", pn.pane.Bokeh(fig))
     #tab2 = ("Horizontal Bar chart", pn.pane.Bokeh(fig))
@@ -251,15 +241,9 @@ def update(target, event, charttype=1):
     target.object = fig
     
     #row2.objects[ob_index] = tabs
-    # Bokeh layout
-    #tab1 = Panel(child=fig, title="Vertical Bar chart")
-    #tab2 = Panel(child=fig, title="Horizontal Bar chart")
-    #tab3 = Panel(child=fig, title="Pie chart")
-    #tabs = Tabs(tabs=[tab1, tab2, tab3])
-    #row2.children[ob_index] = tabs
-    #add_legend_at(fig)
 
-def update_corr(target, event):#*events):
+
+def update_corr(target, event):
     """Update the correlation plot"""
 
     print(event)
@@ -267,14 +251,9 @@ def update_corr(target, event):#*events):
     df, display_options, marker_scale = select_data_corr()    
     fig_corr = bokeh_corr_plot(df, **display_options)
     leg_corr = create_legend_corr(fig_corr, colors=df.data['color'], scale_m=marker_scale)
-    #fig_corr.margin = MARGINS
 
     target.object = fig_corr
-    #row3.objects[ob_index] = pn.pane.Bokeh(fig_corr)
-    #row3.objects[ob_index+1] = pn.pane.Bokeh(leg_corr)
-    # bokeh layout
-    #row3.children[ob_index] = fig_corr
-    #row3.children[ob_index+1] = leg_corr
+
 
 
 
@@ -290,19 +269,14 @@ fig2 = pn.pane.Bokeh(bokeh_barchart(start_display_data, y=y_keys, factors=y_keys
 fig3 = pn.pane.Bokeh(bokeh_barchart(start_display_data, y=y_keys, factors=y_keys, legend_labels=y_keys, 
     fill_color=fill_colors, title=f'Q: {question}', orientation='vertical'))#factors=start_display_data.data['factors'])
 
-#fig.margin = MARGINS
 start_corr_data, display_options, marker_scale = select_data_corr()
 fig_corr_1 = bokeh_corr_plot(start_corr_data, **display_options)
 fig_corr = pn.pane.Bokeh(fig_corr_1)
 leg_corr = pn.pane.Bokeh(create_legend_corr(fig_corr_1, colors=start_corr_data.data['color'], scale_m=marker_scale))
-#fig_corr.margin = MARGINS
-#fig2 = bokeh_barchart(source_corr)
-
-#fig2 = bokeh_multi_scatter(source2, marker_size=[int(x)+6 for x in xs[1])
 
 
 def generate_bar_controls():
-    controls_bar = [question_select, multi_choice]
+    controls_bar = [question_select, multi_choice, multi_choice_method]
     return controls_bar
 
 def generate_corr_controls():
@@ -310,7 +284,7 @@ def generate_corr_controls():
     return controls_corr
 
 md_text_global_filter = ("# Global data filters\n Select some data filters to apply. "
-                         "YYou can filter for research area and or data generation method.")
+                         "You can filter by research area and or by data generation method.")
 
 md_text_barchart = ("# Overview\n Use this area to explorer the results for each survey question. "
                     "Use the dropdown menus to select the question of interest.")
@@ -320,18 +294,19 @@ md_text_corrchart = ("# Basic Correlations\n Find out about basic correlation of
 
 md_text_button = ("# Further charts\n")
 
-# Dashboard layout
+####################
+# Dashboard layout #
+####################
 
-# for spaces: https://docs.bokeh.org/en/latest/docs/reference/models/layouts.html#spacer
 controls_bar = generate_bar_controls()
 for control in controls_bar:
     #control.param.watch(update, ['value'], onlychanged=True)
     control.link(fig, callbacks={'value': update})
     control.link(fig2, callbacks={'value': update})
     control.link(fig3, callbacks={'value': update})
-    
-    #bokeh
-    #control.on_change('value', lambda attr, old, new: update())
+    #control.link(tabs, callbacks={'value': update})
+    control.link(fig_corr, callbacks={'value': update_corr})
+
 tab1 = ("Vertical Bar chart", fig)
 tab2 = ("Horizontal Bar chart", fig2)
 tab3 = ("Pie chart", fig3)
@@ -339,44 +314,19 @@ tabs = construct_tabs([tab1, tab2, tab3])
 
 controls_corr = generate_corr_controls()
 for control in controls_corr:
-    #control.param.watch(update_corr, ['value'], onlychanged=True)
     control.link(fig_corr, callbacks={'value': update_corr})
-    #bokeh
-    #control.on_change('value', lambda attr, old, new: update_corr())
-
-'''
-# Layout bokeh only
-inputs = column(*controls_bar, width=800)
-inputs_corr = column(*controls_corr, width=200)
-
-# create tabs
-tab1 = Panel(child=fig, title="Vertical Bar chart")
-tab2 = Panel(child=fig, title="Horizontal Bar chart")
-tab3 = Panel(child=fig, title="Pie chart")
-tabs = Tabs(tabs=[tab1, tab2, tab3])
-
-row1 = column(desc, md_text_global_filter, row(inputs), sizing_mode="scale_both")
-row2 =  row(tabs)
-row3 = row(inputs_corr, fig_corr, leg_corr)
-
-layout = column(row1, md_text_barchart, row2, md_text_corrchart, row3, row(button_bar), sizing_mode="scale_both")
-'''
 
 # Layout with Panel
+# We provide the layout in this explicit way and not all in one list because first we addresed the objects
+# over the rows name, now this may be obsolete
+
 inputs = pn.Column(*controls_bar, width=800)
 inputs_corr = pn.Column(*controls_corr, width=800)
-
 
 row1 = pn.Column(desc, md_text_global_filter, pn.Row(inputs), sizing_mode="scale_both")
 row2 =  pn.Row(tabs)
 row3 = pn.Column(inputs_corr, pn.Row(fig_corr, leg_corr))
 row4 = pn.Row(button_bar)
-layout = pn.Column(row1, md_text_barchart, row2, md_text_corrchart, row3, row4, sizing_mode="scale_both")
+layout = pn.Column(row1, md_text_barchart, row2, md_text_corrchart, row3, md_text_button, row4, sizing_mode="scale_both")
 
 layout.servable()
-#row1 = pn.Row(inputs, tabs, fig_corr, inputs_corr)
-#first = pn.Column(desc, row1, sizing_mode="scale_both")
-#layout = pn.Column(first, pn.Row(button_bar), sizing_mode="scale_both")
-
-#curdoc().add_root(layout)
-#curdoc().title = "HMC Survey Dashboard"
