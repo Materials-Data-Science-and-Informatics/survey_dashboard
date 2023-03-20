3f50fd595cd3da3d156a47215fb84177249841a5
Jens Bröder <j.broeder@fz-juelich.de>
2023-03-20 19:11:44 +0100

    Update displayed text in App in english and German according to issue # 16.

--- survey_dashboard/text_display.py
+++ survey_dashboard/text_display.py
@@ -17,7 +17,7 @@ This is the central place for all text from the dashboard which needs to be tran
 md_text_description = {'EN': ("Explorer interactively the HMC 2021 Survey results. "
     "The overview sections displays some main results, while you can apply some global filters to these. " 
     "Find out what methods, tools, software and standards are applied by others in your research area. "
-    "The question explorer allows for in detail analyzis of a specific question and simple correlations of two questions. "
+    "The question explorer allows for in detail analysis of a specific question and simple correlations of two questions. "
     "Interact with the visualizations and widgets on the left to explorer the data.\n"
     "The corresponding Data publication of the survey is available [here](https://doi.org/10.7802/2433).\n"
     "\n "
@@ -33,36 +33,35 @@ md_text_description = {'EN': ("Explorer interactively the HMC 2021 Survey result
 
 
 ## Global data filters\n 
-md_text_global_filter = {'EN': ("Select some data filters to apply. "
-                         "You can filter global by research area and or by data generation method."),
-                         'DE': ("Wähle Datenfilter aus. Die Resultate können global nach "
-                         "Forschungsfeld und Erzeugungsmethode gefiltert werden.")}
+md_text_global_filter = {'EN': ("Apply filters to displayed data according to research areas and data generation method"),
+                         'DE': ("Wende Filter auf die Resultate an um nur Daten bestimmter "
+                         "Forschungsfelder und Erzeugungsmethode dazustellen.")}
+
+md_text_global_filters_widgets = [{'EN': "Research area", 'DE': "Forschungsfeld"}, {'EN': "Data generation method", 'DE': "Datenerzeugungs Methode"}]
+
 # Overview\n
-md_text_overview = {'EN': ("Birds eye view of survey results related to research data management."
-                    ""),
-                    'DE': ("Überblick")}
+md_text_overview = {'EN': ("Birds eye view of survey results related to research data management."),
+                    'DE': ("Übersicht mit hervorgehobene Fragen zum Forschungsdatenmanagement")}
 
 
-# Basic Correlations\n
-md_text_corrchart = {'EN': ("Find out about basic correlation of two answers, "
-                    "i.e. how many participants provided the same answer for two questions."),
-                    'DE': ("Finde heraus wie Antworten zweier Fragen sich überschneiden. "
-                    "Also wie viele der Befragten gleich auf zwei Fragen geantwortet haben.")}
 
-md_text_button = {'EN': ("# Further charts\n")}
 
 # Tools and Methods\n 
-md_text_tools_used = {'EN': ("Find out about the tools and methods used in the "
-                    "research area and data generation method you filtered for."),
-                    'DE': ("Finde heraus welche Methoden, welche Werkzeuge und welche Software "
-                    "in welchem Anwendungsbereich genutzt wird.")}
+md_text_tools_used = {'EN': ("Discover the scientific methods, used software and repositories of our communities."),
+                    'DE': ("Entdecke die wissenschaftliche Methoden, Werkzeuge, Software und Repositorien welche in unseren Forschungsgemeinschaft "
+                    "genutzt werden.")}
 
 # Question explorer
 
-md_text_barchart = {'EN': ("Use this area to explorer the results for each survey question. "
-                    "Use the dropdown menus to select the question of interest."),
-                    'DE': ("Exploriere hier die Resultate für den Teil der Umfrage welcher dich am meisten interessiert. "
-                    "Benutze die Auswahlwidgets um die Resultate einer bestimmten Frage anzuzeigen.")}
+md_text_barchart = {'EN': ("Select two questions to explore answers and see how they correspond to one another."),
+                    'DE': ("Wähle zwei Fragen aus um deren Anworten zu sehen und wie diese übereinstimmen.")}
+
+md_text_select_widgets = [{'EN': "Question 1", 'DE': "Frage 1"}, {'EN': "Question 2", 'DE': "Frage 2"}]
+# Basic Correlations\n
+md_text_corrchart = {'EN': ("Correspondence of selected questions"),
+                    'DE': ("Korrespondenz der ausgewählten Fragen")}
+
+md_text_button = {'EN': ("# Further charts\n")}
 
 accordion_titles = {
     'EN': ['Global Data Filters', 'Overview', 'Methods, Tools and Software', 'Survey Data Explorer'],
